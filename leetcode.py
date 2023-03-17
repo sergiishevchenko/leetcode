2ccc7602ded79ad0b5df4c2932c6e7fcf5524e7b
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-03-17 10:48:57 +0100

    added ex3

--- leetcode.py
+++ leetcode.py
@@ -400,3 +400,30 @@ class Solution(object):
                     return False
 
         return True
+
+# Day 6
+# 3. Longest Substring Without Repeating Characters
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s == "":
+            return 0
+        start = 0
+        end = 0
+        maxLengthSubstring = 0
+        unique_elements = set()
+
+        while end < len(s):
+            if s[end] not in unique_elements:
+                unique_elements.add(s[end])
+                end += 1
+                maxLengthSubstring = max(maxLengthSubstring, len(unique_elements))
+            else:
+                unique_elements.remove(s[start])
+                start += 1
+        return maxLengthSubstring
+
+
